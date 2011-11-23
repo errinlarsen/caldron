0ce946214e0e0a5646128ea691682fd1cdd88a0c
Errin Larsen <elarsen@insightcards.com>
2011-11-23 07:52:47 -0600

    Remove models we haven't driven out yet

--- app/models/chore_list_entry.rb
+++
@@ -1,8 +0,0 @@
-class ChoreListEntry < ActiveRecord::Base
-  attr_accessible :completed, :notes
-
-  belongs_to :chore_list
-  belongs_to :chore
-
-  validates_inclusion_of :completed, :in => [true, false]
-end
--- spec/factories/chore_list_entries.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :chore_list_entry do
-    completed false
-    notes "MyString"
-  end
-end
