d502a515e60378001a30abc566494539bc592bf3
Errin Larsen <elarsen@insightcards.com>
2011-11-25 10:19:01 -0600

    Added all the unhappy path tests for edit_user_spec and refactored User factories a bit

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,12 @@
 
 FactoryGirl.define do
   factory :user do
+    name "Tester"
+    email "tester@test.com"
+    password "abc123"
+  end
+
+  factory :user_seq do
     sequence(:name) { |n| "Tester#{n}" }
     sequence(:email) { |n| "#{name}@test.com" }
     password "abc123"
