d80847e5fe57360c34f473d3181e40e422524fff
Errin Larsen <elarsen@insightcards.com>
2011-11-30 20:37:32 -0600

    Clean up some syntax

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,10 +33,10 @@ describe User do
     it { should respond_to(:remember_me=) }
   end
 
-  describe "#chore_lists_by_date" do
-    it { should respond_to(:chore_lists_by_date) }
+  describe "#get_chore_lists_by_date" do
+    it { should respond_to(:get_chore_lists_by_date) }
     context "given 6 ChoreLists across 3 different dates" do
-      let(:output) { subject.chore_lists_by_date }
+      let(:output) { subject.get_chore_lists_by_date }
       before do
         date = Time.zone.today
         3.times do
@@ -45,17 +45,17 @@ describe User do
         end
       end
 
-      context "chore_lists_by_date" do
+      context "get_chore_lists_by_date" do
         specify { output.should be_a(Hash) }
         specify { output.should have(3).items }
       end
 
-      context "chore_lists_by_date[some_date]" do
+      context "get_chore_lists_by_date[some_date]" do
         specify { output[Time.zone.today].should be_an(Array) }
         specify { output[Time.zone.today].should have(2).items }
       end
 
-      context "chore_lists_by_date[some_date].values" do
+      context "get_chore_lists_by_date[some_date].values" do
         let(:today) { Time.zone.today }
         specify { output[today].first.should be_a(ChoreList) }
         specify { output[today].first.should satisfy { |cl| cl.date = today }}
