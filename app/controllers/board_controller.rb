f518bd74ff38a6277cd597c9eebcb3efa2e0f621
Errin Larsen <elarsen@insightcards.com>
2011-12-06 00:14:34 -0600

    Add chores to the controller

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -7,11 +7,16 @@ class BoardController < ApplicationController
     list1 = @board.new_list
 
     list1.date = today
-    list1.chores << "Clean your room"
+    list1.title = "Some chore for today"
+    chore1 = list1.new_chore
+    chore1.name = "Clean your room"
+    chore1.write
     list1.post
+    puts list1.inspect
 
-    list2 = @board.new_list(:date => today + 1.day)
-    list2.chores += ["Clean your room", "Feed the dogs", "Wash the car"]
+    list2 = @board.new_list(:date => today + 1.day,
+                            :title => "Tomorrow's chores")
+    # list2.chores += ["Clean your room", "Feed the dogs", "Wash the car"]
     list2.post
   end
 end
