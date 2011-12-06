a4d72f79a3f46919476b3ceb4e14313797067538
Errin Larsen <elarsen@insightcards.com>
2011-12-05 18:05:27 -0600

    Spec out adding lists to board

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -24,5 +24,21 @@ describe Board do
     it "should set the list's board reference to itself" do
       @it.new_list.board.must_equal(@it)
     end
+
+    it "should accept an attribute hash on behalf of the list maker" do
+      list_maker = MiniTest::Mock.new
+      list_maker.expect(:call, @new_list, [{ :foo => 42, :bar => 'baz'}])
+      @it.list_maker = list_maker
+      @it.new_list(:foo => 42, :bar => 'baz')
+      list_maker.verify
+    end
+  end
+
+  describe "#add_list" do
+    it "should add the list to the board" do
+      list = Object.new
+      @it.add_list(list)
+      @it.lists.must_include(list)
+    end
   end
 end
