54fbdb327de7a388fc4ef6d49a8ef0267e8234ef
Errin Larsen <elarsen@insightcards.com>
2011-12-07 05:55:03 -0600

    Fix the ListsController to handle incoming date params correctly

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,6 +4,9 @@ class ListsController < ApplicationController
   end
 
   def create
+    d = Date.new *params[:list].sort.first(3).map(&:last).map(&:to_i)
+    params[:list].reject! { |key, val| key =~ /^date/ }
+    params[:list][:date] = d
     @list = @board.new_list(params[:list])
     @list.post
     redirect_to root_path, :notice => "List created!"
