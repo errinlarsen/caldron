96d2c055ed9b3d9949e23f5bea952609a0ee4428
Errin Larsen <elarsen@insightcards.com>
2011-12-07 05:24:09 -0600

    Add create action to ListsController

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,10 @@ class ListsController < ApplicationController
   def new
     @list = @board.new_list
   end
+
+  def create
+    @list = @board.new_list(params[:list])
+    @list.post
+    redirect_to root_path, :notice => "List created!"
+  end
 end
